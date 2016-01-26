4b2287378e93e1667466277139b6e53a22000ee6
xavizalote <xavier.cazalot@gmail.com>
2016-01-27 00:02:55 +0100

    ping strictly server side + get videos method

--- server/methods.js
+++ server/methods.js
@@ -1,27 +1,30 @@
 Meteor.methods({
 	pingYoutube: function() {
-		let youtubeData = Youtube.fetchStartupfood(1),
-			lastVideoUploaded = youtubeData.items[0].snippet, // check youtube api docs for more infos about that
-			totalVideosResults = youtubeData.pageInfo.totalResults;
+		// do all stuff
+		if (Meteor.isServer) {
+			let youtubeData = Youtube.fetchStartupfood(1),
+				lastVideoUploaded = youtubeData.items[0].snippet, // check youtube api docs for more infos about that
+				totalVideosResults = youtubeData.pageInfo.totalResults;
 
-		let videosRegistered = Startupfood.find({}, {sort: {publishedAt: 1}}).fetch(),
-				lastVideoRegistered = videosRegistered[0];
+			let videosRegistered = Startupfood.find({}, {sort: {publishedAt: 1}}).fetch(),
+					lastVideoRegistered = videosRegistered[0];
 
-		// the collection is empty or a new video has been uploaded, do the maths
-		if (videosRegistered.length === 0 || Date.parse(lastVideoUploaded.publishedAt) > Date.parse(lastVideoRegistered.publishedAt)) {
-			// log a new video document in the collection
-			Youtube.insertNewVideo(lastVideoUploaded);
+			// the collection is empty or a new video has been uploaded, do the maths
+			if (videosRegistered.length === 0 || Date.parse(lastVideoUploaded.publishedAt) > Date.parse(lastVideoRegistered.publishedAt)) {
+				// log a new video document in the collection
+				Youtube.insertNewVideo(lastVideoUploaded);
 
-			// send to each team connected the new video
-			Slack.sendToTeams({
-				title: lastVideoUploaded.title,
-				youtubeId: lastVideoUploaded.resourceId.videoId,
-				totalVideosResults: totalVideosResults,
-				description: lastVideoUploaded.description
-			});
+				// send to each team connected the new video
+				Slack.sendToTeams({
+					title: lastVideoUploaded.title,
+					youtubeId: lastVideoUploaded.resourceId.videoId,
+					totalVideosResults: totalVideosResults,
+					description: lastVideoUploaded.description
+				});
 
-		} else {
-			console.log('no new video uploaded yet!');
+			} else {
+				console.log('no new video uploaded yet!');
+			}
 		}
 	},
 	requestSlackToken: function(code) {
@@ -59,6 +62,11 @@ Meteor.methods({
 			}
 		} catch(oauthError) {
 			console.log(oauthError);
+		}
+	},
+	getVideos: function() {
+		return {
+			videos: Startupfood.find().fetch()
 		}
 	}
 });
