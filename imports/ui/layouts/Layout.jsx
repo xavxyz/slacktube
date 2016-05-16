1213b32cdc5402b00eb7d201204720f65bb2f19e
xavizalote <xavier.cazalot@gmail.com>
2016-05-16 18:00:04 +0200

    add logo

--- imports/ui/layouts/Layout.jsx
+++ imports/ui/layouts/Layout.jsx
@@ -5,6 +5,7 @@ import { Footer } from '../components/Footer.jsx';
 const Layout = ({ content }) => {
   return (
     <div className="row">
+      <img className="logo" src="slacktube.png" title="Slacktube: from Youtube to Slack" />
       { content }
       <Footer />
     </div>
